bf2e3ee28e457dd1fd049c7e15d492036d20e61b
QuanNguyen176 <quan.ng>
2026-07-02 21:24:26 +0700

    ngon

--- firebase-config.js
+++ firebase-config.js
@@ -2,8 +2,13 @@ window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyCN5uUhRzp59TYsPJAcNTjjoOXWxCo264g",
   authDomain: "fallinloveqq.firebaseapp.com",
   projectId: "fallinloveqq",
-  storageBucket: "fallinloveqq.firebasestorage.app",
   messagingSenderId: "1060231860119",
   appId: "1:1060231860119:web:02484a7ee4c54d9e653ed2",
   measurementId: "G-1J19QZCJ9X"
+};
+
+window.CLOUDINARY_CONFIG = {
+  cloudName: "v8i5rbkk",
+  uploadPreset: "quannguyen",
+  folder: "timelineUploads"
 };
